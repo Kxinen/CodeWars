272b59696b6ef9abd635a257bde0f779076c2fc4
Kainen Rowe <rkainen@gmail.com>
2022-06-18 17:00:11 -0400

    2 new codewars solutions

--- solution001.js
+++ solution001.js
@@ -1,2 +1,10 @@
 
-alert('cool')
+//codewars
+//Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+
+
+
+function roundToNext5(n){
+    let decimalValue = n / 5
+    return Math.ceil(decimalValue) * 5
+  }
